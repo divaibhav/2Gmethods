bff2e2f3df0ad53f7b15f44f0fa1c5bf2fad8c1b
Vaibhav Diwan <divaibhav@gmail.com>
2020-07-28 13:19:14 +0530

    implemented MethodDemo.java, and defined display method.

--- src/MethodDemo.java
+++ src/MethodDemo.java
@@ -5,5 +5,21 @@
  *  Time: 1:07 PM
  */
 
+//creating a method to print hello world. That method does not accept any argument and will not return any value.
 public class MethodDemo {
+    public static void main(String[] args) {
+        System.out.println("method demo");
+        // creating object to call method, as display is in this class
+        // but it is non static mehtod so will be called by object
+        MethodDemo object = new MethodDemo();
+        //calling a method
+        object.display();
+        System.out.println("exiting....");
+
+    }
+    //declaring and defining out method display
+
+    public void display() {
+        System.out.println("hello world");
+    }
 }
